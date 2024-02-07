0f780d1b2c77e9d93b21eb4ce126e0ccefc0639b
Aryan Malik <aryanhunny98@gmail.com>
2024-02-07 23:35:33 +0530

    Implemented write contract state

--- src/context/write_contract.js
+++ src/context/write_contract.js
@@ -5,5 +5,48 @@ import { ethers } from 'ethers'
 // INTENAL IMPORT
 import { CrowdFundingABI, CrowdFundingAddress } from './constants'
 
-import { useWriteContract, useAccount } from 'wagmi'
+import { useWriteContract, useAccount, useSendTransaction, useReadContract } from 'wagmi'
+
+const { writeContract } = useWriteContract()
+const { sendTransaction } = useSendTransaction()
+
+export const createCampaign = (campaign) => {
+    const {title, description, amount, deadline} = campaign
+    const { address } = useAccount()
+    writeContract({
+        abi: CrowdFundingABI,
+        address: CrowdFundingAddress,
+        functionName: 'createCampaign',
+        args: [
+            address, // owner
+            title, // title
+            description, // description
+            ethers.utils.parseUtils(amount, 18), // amount
+            new Date(deadline).getTime() // deadline
+        ]
+    })
+}
+
+export const donate = (pId, amount) => {
+    writeContract({
+        abi: CrowdFundingABI,
+        address: CrowdFundingAddress,
+        functionName: 'donate',
+        args: [pId]
+    })
+    sendTransaction({
+        to: CrowdFundingAddress,
+        value: ethers.utils.parseEther(amount)
+    })
+
+    const campaignsData = useReadContract({
+        abi: CrowdFundingABI,
+        address: CrowdFundingAddress,
+        functionName: 'getCampaigns'
+    })
+
+    location.reload()
+
+    return campaignsData
+}
 
